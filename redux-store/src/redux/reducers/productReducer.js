0d4233836bfcb0673d879985d3d604c530595f62
kingwangwong <kingngngco@gmail.com>
2023-03-20 15:55:03 -0500

    product detail

--- redux-store/src/redux/reducers/productReducer.js
+++ redux-store/src/redux/reducers/productReducer.js
@@ -7,7 +7,16 @@ const initialState = {
 export const productReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
-            return {...state, products:payload};
+            return { ...state, products: payload };
+        default:
+            return state;
+    }
+};
+
+export const selectedProductReducer = (state = {}, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCT:
+            return { ...state, ...payload }
         default:
             return state;
     }
