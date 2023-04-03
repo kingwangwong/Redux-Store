2de4cd152676998d63aaf4c036dcdd121cb808f0
kingwangwong <kingngngco@gmail.com>
2023-04-03 17:10:34 -0500

    added product detail

--- redux-store/src/App.js
+++ redux-store/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 import Header from './containers/Header';
 import ProductListing from './containers/ProductListing';
 import ProductDetail from './containers/ProductDetail';
@@ -9,11 +9,11 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Switch>
-          <Route path="/"  exact component={ProductListing} />
-          <Route path="/product/:productId"  component={ProductDetail} />
+        <Routes>
+          <Route path="/"  exact element={<ProductListing/>} />
+          <Route path="/product/:productId"  element={<ProductDetail/>} />
           <Route>404 Not Found </Route>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
--- redux-store/src/index.js
+++ redux-store/src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { StrictMode } from "react";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </StrictMode>
+)
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
